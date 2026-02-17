fa9a8e603ae89ac6fcf582dd37070da37e69931e
Goleath31 <149979774+Goleath31@users.noreply.github.com>
2026-02-17 12:15:54 -0700

    Add files via upload

---
+++ Proyectopanaderia/src/main/java/proyectopanaderia/Proyectopanaderia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyectopanaderia;
+
+/**
+ *
+ * @author golea
+ */
+public class Proyectopanaderia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
