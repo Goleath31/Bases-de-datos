bbf38664b71d8490d9f3cc711d0af419cc0925ef
jruben28 <140290443+jruben28@users.noreply.github.com>
2026-02-23 13:05:36 -0700

    clase detalle pedido

---
+++ Proyectopanaderia/src/main/java/persistencia/dominio/DetallePedido.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package persistencia.dominio;
+
+/**
+ *
+ * @author joser
+ */
+public class DetallePedido {
+    private int idDetallePedido;
+    private int idPedido;
+    private int idProducto;
+    private int cantidad;
+    private String notas;
+    private float subtotal;
+
+    public DetallePedido(int idDetallePedido, int idPedido, int idProducto, int cantidad, String notas, float subtotal) {
+        this.idDetallePedido = idDetallePedido;
+        this.idPedido = idPedido;
+        this.idProducto = idProducto;
+        this.cantidad = cantidad;
+        this.notas = notas;
+        this.subtotal = subtotal;
+    }
+    
+    public DetallePedido(int idDetallePedido, int idPedido, int idProducto, int cantidad, String notas) {
+        this.idDetallePedido = idDetallePedido;
+        this.idPedido = idPedido;
+        this.idProducto = idProducto;
+        this.cantidad = cantidad;
+        this.notas = notas;
+    }
+
+    public DetallePedido(int idPedido, int idProducto, int cantidad, String notas, float subtotal) {
+        this.idPedido = idPedido;
+        this.idProducto = idProducto;
+        this.cantidad = cantidad;
+        this.notas = notas;
+        this.subtotal = subtotal;
+    }
+
+    public DetallePedido(int idPedido, int idProducto, int cantidad, String notas) {
+        this.idPedido = idPedido;
+        this.idProducto = idProducto;
+        this.cantidad = cantidad;
+        this.notas = notas;
+    }
+
+    
+    
+    
+    
+
+    public int getIdDetallePedido() {
+        return idDetallePedido;
+    }
+
+    public void setIdDetallePedido(int idDetallePedido) {
+        this.idDetallePedido = idDetallePedido;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getNotas() {
+        return notas;
+    }
+
+    public void setNotas(String notas) {
+        this.notas = notas;
+    }
+
+    public float getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(float subtotal) {
+        this.subtotal = subtotal;
+    }
+    
+    
+}
